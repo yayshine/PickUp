2f5119b2c1791da43e53d502d8ba8319b82b433d
Yanqing Dai <yd2369@columbia.edu>
2017-03-23 18:22:35 -0400

    correctly linked to firebase

--- app/src/main/java/com/mobile/pickup/MainActivity.java
+++ app/src/main/java/com/mobile/pickup/MainActivity.java
@@ -1,13 +1,34 @@
 package com.mobile.pickup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mFirebaseDatabaseReference;
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUsername;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    // Firebase instance variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
 
-        Firebase ref = new Firebase("https://pickup-64523.firebaseio.com/");
-        ref.child("customer").setValue("codeCustomer");
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
-
+        myRef.setValue("Hello, hello!");
     }
-
 
 }
