bea6c1793ae2de6a3776ea45ef4ab04c979ce296
yayshine <ys2657@columbia.edu>
2017-03-24 01:02:32 -0400

    Added OrderActivity

---
+++ app/src/main/java/com/mobile/pickup/Customer/OrderActivity.java
@@ -0,0 +1,24 @@
+package com.mobile.pickup.Customer;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mobile.pickup.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    FragmentManager mFragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentManager.beginTransaction()
+                .replace(R.id.container, new VendorListFragment())
+                .commit();
+    }
+
+}
