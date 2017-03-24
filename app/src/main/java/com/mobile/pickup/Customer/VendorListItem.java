f3a559a281ed8ee94577e944e51c89d4095158b1
yayshine <ys2657@columbia.edu>
2017-03-24 00:57:07 -0400

    Created view for VendorList item

---
+++ app/src/main/java/com/mobile/pickup/Customer/VendorListItem.java
@@ -0,0 +1,20 @@
+package com.mobile.pickup.Customer;
+
+/**
+ * Created by yayshin on 3/23/17.
+ */
+
+public class VendorListItem {
+    public int id = -1;
+    public String name;
+    public int waitTime;
+
+    public VendorListItem(){
+    }
+
+    public VendorListItem(int _id, String name, int waitTime){
+        this.id = _id;
+        this.name = name;
+        this.waitTime = waitTime;
+    }
+}
---
+++ app/src/main/java/com/mobile/pickup/Customer/VendorListItemView.java
@@ -0,0 +1,29 @@
+package com.mobile.pickup.Customer;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.mobile.pickup.R;
+
+/**
+ * Created by yayshin on 3/23/17.
+ */
+
+public class VendorListItemView extends FrameLayout {
+    TextView text_name;
+    TextView text_wait_time;
+
+    public VendorListItemView(Context context){
+        super(context);
+
+        inflate(getContext(), R.layout.view_vendor_list_item, this);
+        text_name = (TextView)findViewById(R.id.text_name);
+        text_wait_time = (TextView)findViewById(R.id.text_wait_time);
+    }
+
+    public void setData(VendorListItem foodtruck){
+        text_name.setText(foodtruck.name);
+        text_wait_time.setText(Integer.toString(foodtruck.waitTime));
+    }
+}
