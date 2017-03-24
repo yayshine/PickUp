8f0c3b9d4dc94e7b9d8b6ae2c72cd1342194c12e
yayshine <ys2657@columbia.edu>
2017-03-24 04:05:09 -0400

    Edited AndroidManifest bc of changed file location

---
+++ app/src/main/java/com/mobile/pickup/Controller/CustomerSide/Menu/MenuAdapter.java
@@ -0,0 +1,8 @@
+package com.mobile.pickup.Controller.CustomerSide.Menu;
+
+/**
+ * Created by yayshin on 3/24/17.
+ */
+
+public class MenuAdapter {
+}
---
+++ app/src/main/java/com/mobile/pickup/View/CustomerSide/Menu/FoodItemView.java
@@ -0,0 +1,8 @@
+package com.mobile.pickup.View.CustomerSide.Menu;
+
+/**
+ * Created by yayshin on 3/24/17.
+ */
+
+public class FoodItemView {
+}
