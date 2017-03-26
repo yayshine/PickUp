56ca4ebf3c7e2142bc917fa95c07e9d3662341e6
yayshine <ys2657@columbia.edu>
2017-03-26 08:24:29 -0400

    Created MenuManager.getAllFoodItems to move all the Firebase calls in MenuFragment into this; Implemented OnFoodItemsReadListener interface

--- app/src/main/java/com/mobile/pickup/MenuManager.java
+++ app/src/main/java/com/mobile/pickup/MenuManager.java
@@ -1,16 +1,26 @@
 package com.mobile.pickup;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mobile.pickup.Model.FoodItem;
 import com.mobile.pickup.Model.Menu;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Yanqing on 3/24/17.
  */
 
 public class MenuManager {
+    private OnFoodItemsReadListener mListener;
 
     public Menu addMenu(String vendorID, HashMap<String, Boolean> foodItemIDList){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -50,5 +60,41 @@ public class MenuManager {
         DatabaseReference menuRef = database.getReference("Menu");
 
         menuRef.child(menuID).child("foodItemIDList").child(foodItemID).setValue(false);
+    }
+
+    public List<FoodItem> getAllFoodItems(final String menuID){
+        DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        final List<FoodItem> foodItems = new ArrayList<>();
+
+        ValueEventListener menuListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Menu menu = dataSnapshot.child("Menu").child(menuID).getValue(Menu.class);
+
+                Set<String> foodItemIDList = menu.getFoodItemIDVisibilityMap().keySet();
+                for(String foodItemID : foodItemIDList){
+                    if(menu.getFoodItemIDVisibilityMap().get(foodItemID) == true){
+                        FoodItem foodItem = dataSnapshot.child("FoodItem").child(foodItemID).getValue(FoodItem.class);
+                        foodItems.add(foodItem);
+                    }
+                }
+                mListener.onFinish();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("getMenu", "Display menu failed.");
+            }
+        };
+        mDatabaseReference.addValueEventListener(menuListener);
+        return foodItems;
+    }
+
+    public void setOnFoodItemsReadListener(OnFoodItemsReadListener listener) {
+        mListener = listener;
+    }
+
+    public interface OnFoodItemsReadListener{
+        void onFinish();
     }
 }
