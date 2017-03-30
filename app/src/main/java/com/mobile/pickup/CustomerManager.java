d80b8bb53d066b577c5a69076b6500d05c56137c
Yanqing Dai <yd2369@columbia.edu>
2017-03-30 10:29:43 -0400

    CustomerManager added

---
+++ app/src/main/java/com/mobile/pickup/CustomerManager.java
@@ -0,0 +1,29 @@
+package com.mobile.pickup;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mobile.pickup.Model.Customer;
+
+/**
+ * Created by Yanqing on 3/30/17.
+ */
+
+public class CustomerManager {
+
+    public static Customer addCustomer(String customerName){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference customerRef = database.getReference("Customer");
+
+        String customerID = customerRef.push().getKey();
+        Customer nCustomer = new Customer(customerID,customerName);
+        customerRef.child(customerID).setValue(nCustomer);
+
+        return nCustomer;
+    }
+
+    public static void removeCustomer(String customerID){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference customerRef = database.getReference("Customer");
+        customerRef.child(customerID).removeValue();
+    }
+}
