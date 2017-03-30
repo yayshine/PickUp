c29042ed735fb233e9bede2cb40d2c14bf4104ec
yayshine <ys2657@columbia.edu>
2017-03-30 09:54:52 -0400

    Add and Implement TempMainActivity

---
+++ app/src/main/java/com/mobile/pickup/Controller/TempMainActivity.java
@@ -0,0 +1,29 @@
+package com.mobile.pickup.Controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mobile.pickup.R;
+
+public class TempMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_main);
+
+        Button btn_customer = (Button)findViewById(R.id.btn_customer);
+        btn_customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TempMainActivity.this, TempLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_vendor = (Button)findViewById(R.id.btn_vendor);
+    }
+}
