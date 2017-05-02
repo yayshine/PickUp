07ac8cdd1f351c5428e76877585a9aafa35bd523
yayshine <ys2657@columbia.edu>
2017-05-02 15:22:04 -0400

    Temp push

--- app/src/main/java/com/mobile/pickup/Controller/CustomerSide/OrderActivity.java
+++ app/src/main/java/com/mobile/pickup/Controller/CustomerSide/OrderActivity.java
@@ -1,9 +1,12 @@
 package com.mobile.pickup.Controller.CustomerSide;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.mobile.pickup.Controller.CustomerSide.Menu.MenuFragment;
 import com.mobile.pickup.Controller.CustomerSide.VendorList.VendorListFragment;
@@ -13,7 +16,11 @@ import com.mobile.pickup.Model.CustomerSide.TempOrder;
 import com.mobile.pickup.Model.Order;
 import com.mobile.pickup.R;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
+
+    NavigationView naviView;
+
     public static final String TAG_VENDOR_LIST = "VENDOR_LIST";
     public static final String TAG_MENU = "MENU";
 
@@ -32,6 +39,9 @@ public class OrderActivity extends AppCompatActivity {
         mFragmentManager.beginTransaction()
                 .replace(R.id.container, new VendorListFragment())
                 .commit();
+
+        naviView = (NavigationView)findViewById(R.id.navi);
+        naviView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -43,5 +53,22 @@ public class OrderActivity extends AppCompatActivity {
             mTempOrder = new TempOrder();
         }
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.menu_item1 :
+            case R.id.menu_item2 :
+            case R.id.menu_item3 :
+                menuItem.setChecked(true);
+                Toast.makeText(this, "Check Item Select : " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_sub1 :
+            case R.id.menu_sub2 :
+                Toast.makeText(this, "Sub Item Click : " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
     }
 }
