85337808c8265a4e397351e3521e94a2dfe174a8
Yanqing Dai <yd2369@columbia.edu>
2017-05-02 01:59:25 -0400

    Commentted out redirect to VendorList part to allow sign out page to show up now

--- app/src/main/java/com/mobile/pickup/Controller/CustomerSide/TempLoginActivity.java
+++ app/src/main/java/com/mobile/pickup/Controller/CustomerSide/TempLoginActivity.java
@@ -291,10 +291,11 @@ public class TempLoginActivity extends AppCompatActivity implements View.OnClick
             propertyManager.setUsername(customer.getCustomerName());
             propertyManager.setID(customer.getID());
 
-            Intent intent = new Intent(TempLoginActivity.this, OrderActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            
+//      if comment out these lines, will redirect to vendor list
+//            Intent intent = new Intent(TempLoginActivity.this, OrderActivity.class);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(intent);
+
         } else {
             mStatusTextView.setText(R.string.signed_out);
             mDetailTextView.setText(null);
